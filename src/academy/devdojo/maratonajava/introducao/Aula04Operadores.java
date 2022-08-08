458ee26abb1e1a7163101296b7f0ac6336e598a4
João Paulo <jpmaziero@gmail.com>
2022-08-08 09:51:07 -0300

    Maratona-Java video 16 Operadores

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -2,10 +2,28 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula04Operadores {
     public static void main(String[] args) {
+        // + - /*
         int numero1 = 10;
         int numero2 = 20;
-        double resultado = numero1 /  numero2;
+        double resultado = numero1 / (double) numero2;
         System.out.println( resultado );
+
+        // %
+        int resto = 21 % 7 ;
+        System.out.println(resto);
+
+        // < > <= >=  ==  !=
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezMaiorQueVinte "+isDezMenorQueVinte);
+        System.out.println("isDezMaiorQueVinte "+isDezIgualVinte);
+        System.out.println("isDezIgualDez "+isDezIgualDez);
+        System.out.println("isDezDiferenteDez "+isDezDiferenteDez);
 
     }
 }
