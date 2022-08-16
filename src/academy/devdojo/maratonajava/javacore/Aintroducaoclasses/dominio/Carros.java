aaf8d53a3235e164d09e5c75d771dac41a739222
João Paulo <jpmaziero@gmail.com>
2022-08-16 17:12:45 -0300

    Maratona-Java video 42 Orientação a Objetos - Rxercício Classes Carro Test

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Carros.java
@@ -0,0 +1,6 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
+
+public class Carros {
+    public String nome, modelo;
+    public int ano;
+    }
---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/teste/CarrosTest01.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.teste;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Carros;
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class CarrosTest01 {
+    public static void main(String[] args) {
+        Carros carros = new Carros();
+        carros.nome = "Civic";
+        carros.modelo = "LX";
+        carros.ano = 2022;
+
+        Carros carros2 = new Carros();
+        carros2.nome = "Fusca";
+        carros2.modelo = "1600";
+        carros2.ano = 1976;
+
+        System.out.println("Carro 1 "+carros.nome+ "  Modelo "+carros.modelo+"  Ano "+carros.ano);
+        System.out.println("\n Carro 2 "+carros2.nome+ "  Modelo "+carros2.modelo+"  Ano "+carros2.ano);
+    }
+}
