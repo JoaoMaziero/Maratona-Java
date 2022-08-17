c00cc030160cbccf445fa51e645330f2a1ed855b
João Paulo <jpmaziero@gmail.com>
2022-08-17 10:19:37 -0300

    Maratona-Java video 47 Orientação a Objetos - Exercício Métodos pt03 Retorno pt2

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -1,22 +1,39 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Calculadora {
-    public void somarDoisNumeros () {
-        System.out.println(10+10);
-    }
-    public void subtraiWoisNumeros (){
-        System.out.println(21-2);
-    }
-    public void multiplicaDoisNumeros(int num1, int num2){
-        System.out.println(num1*num2);
+    public void somarDoisNumeros() {
+        System.out.println(10 + 10);
     }
 
-    public double divideDoisNumeros(double num1, double num2){
-        if (num2 == 0){
-            return  0;
+    public void subtraiWoisNumeros() {
+        System.out.println(21 - 2);
+    }
+
+    public void multiplicaDoisNumeros(int num1, int num2) {
+        System.out.println(num1 * num2);
+    }
+
+    public double divideDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
+            return 0;
         }
 
-        return num1/num2;
+        return num1 / num2;
+    }
+
+    public double divideDoisNumeros2(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        }
+        return 0;
+    }
+
+    public void imprimeDivisaoDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Não existe divisão por 0");
+            return;
+        }
+        System.out.println(num1/num2);
     }
 
 }
