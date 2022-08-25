04423d3af501bf13b07ee519a2cb7cb3506f7498
João Paulo <jpmaziero@gmail.com>
2022-08-25 14:15:19 -0300

    Maratona-Java video 55 Orientação a Objetos - Modificador de acesso private, get e set pt03

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -2,9 +2,10 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Funcionario {
 
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media ;
 
     public void imprime() {
         if (salarios == null) {
@@ -32,6 +33,34 @@ public class Funcionario {
         media /= salarios.length;
         System.out.println("\nMedia salaria " + media);
 
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public double getMedia() {
+        return media;
     }
 
 }
